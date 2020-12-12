8ef2eff589d50c3846f691329d4b96af1278b8c9
Chip Hogg <charles.r.hogg@gmail.com>
2020-12-12 07:03:28 -0500

    Problem 12, part 2

--- 12.py
+++ 12.py
@@ -7,17 +7,47 @@ import aoc
 def main():
     harness = aoc.Harness()
     harness.attempt_part(
-        _compute_manhattan_distance, "./12.txt", [("./12_test.txt", 25)],
+        _action_manhattan_distance, "./12.txt", [("./12_test.txt", 25)],
+    )
+    harness.attempt_part(
+        _waypoint_manhattan_distance, "./12.txt", [("./12_test.txt", 286)],
     )
 
 
-def _compute_manhattan_distance(filename):
+def _action_manhattan_distance(filename):
     position = np.array([0, 0])
     direction = np.array([1, 0])
     with open(filename) as f:
         for line in f.read().splitlines():
             _apply_instruction(position, direction, line)
     return abs(position[0]) + abs(position[1])
+
+
+def _waypoint_manhattan_distance(filename):
+    position = np.array([0, 0])
+    waypoint = np.array([10, 1])
+    with open(filename) as f:
+        for line in f.read().splitlines():
+            _apply_waypoint_instructions(position, waypoint, line)
+    return abs(position[0]) + abs(position[1])
+
+
+def _apply_waypoint_instructions(position, waypoint, line):
+    move, size = line[0], int(line[1:])
+    if move == "R":
+        _turn_right(waypoint, size // 90)
+    if move == "L":
+        _turn_right(waypoint, 4 - (size // 90))
+    if move == "F":
+        position += waypoint * size
+    if move == "N":
+        waypoint += np.array([0, size])
+    if move == "S":
+        waypoint += np.array([0, -size])
+    if move == "E":
+        waypoint += np.array([size, 0])
+    if move == "W":
+        waypoint += np.array([-size, 0])
 
 
 def _apply_instruction(position, direction, line):
