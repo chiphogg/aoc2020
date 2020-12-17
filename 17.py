79cb670796946ec75b9b200d2f8d8aa2203d23db
Chip Hogg <charles.r.hogg@gmail.com>
2020-12-17 11:32:15 -0500

    Problem 17, part 2

--- 17.py
+++ 17.py
@@ -9,20 +9,31 @@ import conway
 def main():
     harness = aoc.Harness()
     harness.attempt_part(
-        _count_active_after_6, "./17.txt", [("./17_test.txt", 112)],
+        _count_active_3D_after_6, "./17.txt", [("./17_test.txt", 112)],
+    )
+    harness.attempt_part(
+        _count_active_4D_after_6, "./17.txt", [("./17_test.txt", 848)],
     )
 
 
-def _count_active_after_6(filename):
-    cube = conway.ConwayCube(_initial_state(filename))
-    for _ in range(6):
+def _count_active_4D_after_6(filename):
+    return _count_active_after(data=np.array([[_initial_state(filename)]]), n=6)
+
+
+def _count_active_3D_after_6(filename):
+    return _count_active_after(data=np.array([_initial_state(filename)]), n=6)
+
+
+def _count_active_after(data, n):
+    cube = conway.ConwayCube(data)
+    for _ in range(n):
         cube = cube.execute_bootup_cycle()
     return cube.count_active()
 
 
 def _initial_state(filename):
     with open(filename) as f:
-        return np.array([[list(line) for line in f.read().splitlines()]])
+        return [list(line) for line in f.read().splitlines()]
 
 
 if __name__ == "__main__":
