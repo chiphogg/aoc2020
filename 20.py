8a6195454005d5167119eee84db8979a14298c6f
Chip Hogg <charles.r.hogg@gmail.com>
2020-12-20 12:54:19 -0500

    Problem 20, part 2

--- 20.py
+++ 20.py
@@ -12,6 +12,40 @@ def main():
     harness.attempt_part(
         _prod_corner_ids, "./20.txt", [("./20_test.txt", 20899048083289)],
     )
+    harness.attempt_part(
+        _remove_seamonster_and_count_active, "./20.txt", [("./20_test.txt", 273)],
+    )
+
+
+def _remove_seamonster_and_count_active(filename):
+    tiles = _read_tiles(filename)
+    square = _make_square(tiles)
+    assert _solve_square(tiles, square)
+
+    composite = Composite(tiles, square)
+    for _ in range(2):
+        for _ in range(4):
+            if _found_any_seamonsters(composite):
+                return composite.count_active()
+            composite.turn()
+        composite.flip()
+    return None
+
+
+def _found_any_seamonsters(composite):
+    seamonster_spots = _seamonster_spots()
+    found_any = False
+
+    for row in range(composite.total_size - 2):
+        for col in range(composite.total_size - 19):
+            if all(
+                composite.get((row + i, col + j)) == "#" for i, j in seamonster_spots
+            ):
+                found_any = True
+                for i, j in seamonster_spots:
+                    composite.set((row + i, col + j), "O")
+
+    return found_any
 
 
 def _prod_corner_ids(filename):
@@ -125,6 +159,9 @@ class ImageTile:
     def get(self, i_tuple):
         return self.data[self.coords.indices(i_tuple)]
 
+    def set(self, i_tuple, value):
+        self.data[self.coords.indices(i_tuple)] = value
+
     def turn(self):
         self.coords.turn()
 
@@ -154,6 +191,64 @@ class Orientation:
 
     def _origin(self):
         return (self.n - 1) * np.array([((self.turned + 1) // 2) % 2, self.turned // 2])
+
+
+class Composite:
+    def __init__(self, tiles, square):
+        self.tiles = tiles
+        self.square = square
+        self.tile_size = aoc.assume_all_identical(tiles[i].size - 2 for i in tiles)
+        self.square_size = aoc.assume_all_identical(square.shape)
+        self.total_size = self.tile_size * self.square_size
+        self.coords = Orientation(self.total_size)
+
+    def _square_and_tile_indices(self, i_tuple):
+        i_transformed = self.coords.indices(i_tuple)
+        return (
+            tuple(i // self.tile_size for i in i_transformed),
+            tuple(i % self.tile_size + 1 for i in i_transformed),
+        )
+
+    def get(self, i_tuple):
+        i_square, i_tile = self._square_and_tile_indices(i_tuple)
+        return self.tiles[self.square[i_square]].get(i_tile)
+
+    def set(self, i_tuple, value):
+        i_square, i_tile = self._square_and_tile_indices(i_tuple)
+        self.tiles[self.square[i_square]].set(i_tile, value)
+
+    def count_active(self):
+        return sum(
+            self.get((i, j)) == "#"
+            for i in range(self.total_size)
+            for j in range(self.total_size)
+        )
+
+    def turn(self):
+        self.coords.turn()
+
+    def flip(self):
+        self.coords.flip()
+
+
+def _seamonster_spots():
+    return (
+        (1, 0),
+        (2, 1),
+        (2, 4),
+        (1, 5),
+        (1, 6),
+        (2, 7),
+        (2, 10),
+        (1, 11),
+        (1, 12),
+        (2, 13),
+        (2, 16),
+        (1, 17),
+        (1, 18),
+        (0, 18),
+        (1, 19),
+    )
 
 
 if __name__ == "__main__":
