ed9bb301d38308001b1e9647602b82a71a3bc315
Chip Hogg <charles.r.hogg@gmail.com>
2020-12-23 08:36:03 -0500

    Problem 23, part 2

--- 23.py
+++ 23.py
@@ -8,6 +8,20 @@ def main():
     harness.attempt_part(
         _collect_labels_after_100_moves, "925176834", [("389125467", "67384529")],
     )
+    harness.attempt_part(
+        _multiply_2_labels_after_1e7_moves_1e6_cups,
+        "925176834",
+        [("389125467", 149245887792)],
+    )
+
+
+def _multiply_2_labels_after_1e7_moves_1e6_cups(cup_indices):
+    next_cups = _find_next_cup(cup_indices, total=1_000_000)
+    i = int(cup_indices[0])
+    for _ in range(10_000_000):
+        i = _make_move(next_cups, i)
+    j = next_cups[1]
+    return j * next_cups[j]
 
 
 def _collect_labels_after_100_moves(cup_indices):
@@ -16,15 +30,6 @@ def _collect_labels_after_100_moves(cup_indices):
     for _ in range(100):
         i = _make_move(next_cups, i)
     return "".join(str(c) for c in _cups_after_1(next_cups))
-
-
-def _print(next_cups, current):
-    cups = [f"({current})"]
-    i = current
-    for _ in range(len(next_cups) - 2):
-        i = next_cups[i]
-        cups.append(str(i))
-    print(" ".join(cups))
 
 
 def _make_move(next_cups, current):
@@ -65,10 +70,20 @@ def _cups_after_1(next_cups):
     return results
 
 
-def _find_next_cup(labels):
-    ids = [int(x) for x in labels]
-    next_cup = {ids[i]: ids[(i + 1) % len(ids)] for i in range(len(ids))}
-    return [0] + [next_cup[i + 1] for i in range(len(ids))]
+def _find_next_cup(labels, total=None):
+    highest = total if total else len(labels)
+    next_cup = [0] + [i + 2 for i in range(highest)]
+
+    for i in range(len(labels) - 1):
+        next_cup[int(labels[i])] = int(labels[i + 1])
+
+    if highest > len(labels):
+        next_cup[int(labels[-1])] = len(labels) + 1
+        next_cup[highest] = int(labels[0])
+    else:
+        next_cup[int(labels[-1])] = int(labels[0])
+
+    return next_cup
 
 
 if __name__ == "__main__":
