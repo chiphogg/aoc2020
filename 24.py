45cd8799b97b751bdff7cfb994e12411bbcbf44f
Chip Hogg <charles.r.hogg@gmail.com>
2020-12-24 08:13:07 -0500

    Problem 24, part 2

--- 24.py
+++ 24.py
@@ -11,11 +11,48 @@ def main():
     harness.attempt_part(
         _count_black_tiles, "./24.txt", [("./24_test.txt", 10)],
     )
+    harness.attempt_part(
+        _count_black_after_100_days, "./24.txt", [("./24_test.txt", 2208)],
+    )
+
+
+def _count_black_after_100_days(filename):
+    black = _black_tiles(_count_flips(_read_tile_lines(filename)))
+    for _ in range(100):
+        black = set.symmetric_difference(black, _coords_to_flip(black))
+    return len(black)
+
+
+def _coords_to_flip(black):
+    return {
+        c
+        for c in _all_coords(black)
+        if (c in black and _num_black_neighbours(c, black) not in (1, 2))
+        or (c not in black and _num_black_neighbours(c, black) == 2)
+    }
+
+
+def _all_coords(black):
+    return set.union(
+        black, {(a + i, b + j) for (a, b) in black for (i, j) in _neighbour_coords()}
+    )
+
+
+def _num_black_neighbours(c, black):
+    a, b = c
+    return sum((a + i, b + j) in black for (i, j) in _neighbour_coords())
+
+
+def _neighbour_coords():
+    return ((1, 0), (0, 1), (-1, 1), (-1, 0), (0, -1), (1, -1))
 
 
 def _count_black_tiles(filename):
-    flips = _count_flips(_read_tile_lines(filename))
-    return sum(i % 2 for i in flips.values())
+    return len(_black_tiles(_count_flips(_read_tile_lines(filename))))
+
+
+def _black_tiles(flips):
+    return {i for i in flips if flips[i] % 2}
 
 
 def _count_flips(tile_lines):
